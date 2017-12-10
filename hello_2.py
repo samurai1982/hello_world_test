da29aad29799d48df8914aaa0c58530bb403d5c9
samurai1982 <30388107+samurai1982@users.noreply.github.com>
2017-12-11 02:09:13 +0900

    Add files via upload

---
+++ hello_2.py
@@ -0,0 +1,8 @@
+print("こんにちわ")
+print("今日は何日？")
+my_day = input()
+print ("今日は" + my_day + "日だったね！")
+print ("明日は" + str(int(my_day)+1) + "日だね")
+print("ところでお名前は？")
+my_name = input()
+print("あなたの名前は " + str(len(my_name)) +  "文字ですね")
